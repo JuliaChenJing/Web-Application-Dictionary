75b8e5d9f58d2e2fd65ff54cc70bebe775095719
Julia Chen <juliachenjing@gmail.com>
2017-11-24 11:37:52 +0800

    Update closure.js

--- javaScriptPractice/closure.js
+++ javaScriptPractice/closure.js
@@ -1,5 +1,7 @@
 /*A closure is an inner function that has access to the variables in the outer (enclosing) function’s scope chain. 
-The closure has access to variables in three scopes; specifically: 
+The closure has access to variables in three scopes; 
+
+specifically: 
 (1) variable in its own scope, 
 (2) variables in the enclosing function’s scope, and 
 (3) global variables.
