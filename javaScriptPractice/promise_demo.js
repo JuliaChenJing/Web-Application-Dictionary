a5a39c43b7b09d15b9abc5f014ffbcf6560c5e08
Julia Chen <juliachenjing@gmail.com>
2017-12-08 07:59:51 -0500

    Update promise_demo.js

--- javaScriptPractice/promise_demo.js
+++ javaScriptPractice/promise_demo.js
@@ -1,8 +1,10 @@
-https://www.youtube.com/watch?v=s6SH72uAn3Q&t=29s
+//https://www.youtube.com/watch?v=s6SH72uAn3Q&t=29s
+//https://github.com/JuliaChenJing/Web_Application_Dictionary/blob/master/javaScriptPractice/promise_demo.js
 
 newFunction();
 
 function newFunction() {
+    
     let promiseToCleanTheRoom = new Promise(function (resolve, reject) {
         //cleaning the room
         let isClean = false;
@@ -13,9 +15,10 @@ function newFunction() {
             reject('not Clean');
         }
     });
+
     promiseToCleanTheRoom.then(function (fromResolve) {
         console.log('the room is' + fromResolve); //the room is clean
     }).catch(function (fromReject) {
-        console.log('the room is ' + fromReject);
+        console.log('the room is ' + fromReject);//the room is not clean
     });
 }
