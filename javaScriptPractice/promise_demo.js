b900a7e1fdbe84abbc188774a12d571c8c749c04
Julia Chen <juliachenjing@gmail.com>
2017-12-08 07:52:40 -0500

    Update promise_demo.js

--- javaScriptPractice/promise_demo.js
+++ javaScriptPractice/promise_demo.js
@@ -1,20 +1,20 @@
 //https://github.com/JuliaChenJing/Web_Application_Dictionary/edit/master/javaScriptPractice/promise_demo.js
-let promiseToCleanTheRoom = new Promise(function(resolve, reject) {
-    
-      //cleaning the room
-    
-      let isClean = false;
-    
-      if (isClean) {
-        resolve('Clean');
-      } else {
-        reject('not Clean');
-      }
-    
+newFunction();
+
+function newFunction() {
+    let promiseToCleanTheRoom = new Promise(function (resolve, reject) {
+        //cleaning the room
+        let isClean = false;
+        if (isClean) {
+            resolve('Clean');
+        }
+        else {
+            reject('not Clean');
+        }
     });
-    
-    promiseToCleanTheRoom.then(function(fromResolve) {
-      console.log('the room is' + fromResolve);//the room is clean
-    }).catch(function(fromReject){
-        console.log('the room is ' + fromReject);//the room is not clean
-    })
+    promiseToCleanTheRoom.then(function (fromResolve) {
+        console.log('the room is' + fromResolve); //the room is clean
+    }).catch(function (fromReject) {
+        console.log('the room is ' + fromReject);
+    });
+}
