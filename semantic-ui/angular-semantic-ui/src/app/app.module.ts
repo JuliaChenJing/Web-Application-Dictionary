bf98f7794003721d6ae4351b6c1b91dcb63971b2
Julia Chen <juliachenjing@gmail.com>
2018-01-30 09:49:56 -0500

    ng g c cheat-sheet --spec false

--- semantic-ui/angular-semantic-ui/src/app/app.module.ts
+++ semantic-ui/angular-semantic-ui/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CheatSheetComponent } from './cheat-sheet/cheat-sheet.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheatSheetComponent
   ],
   imports: [
     BrowserModule
